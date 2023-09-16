e3f8a5d6af5ab88c6aaf88956616df88b05b586b
adev07 <adityaanand1245@gmail.com>
2023-09-16 05:39:36 +0530

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,37 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"WHY REACT JS?",
+        tags:["react","gsap","styled-components"],
+        date:"13 May, 2021",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:828/format:webp/1*eiwDa9RrDGIgE5G2424Nuw.jpeg",
+        link:"https://medium.com/catalysts-reachout/why-react-js-1bbd6dea24a7"
+    },
+    {
+        id:2,
+        name:"8 Top UX design trends for 2022",
+        tags:["ux","styled-components","case-studies"],
+        date:"5 July, 2021",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:1400/format:webp/0*-vW4yuQYFYNiva9i.png",
+        link:"https://medium.com/catalysts-reachout/8-top-ux-design-trends-for-2022-151a4235d2b9"
+    },
+    {
+        id:3,
+        name:"How to become successful freelancer in college?",
+        tags:["freelancing","student","internship"],
+        date:"6 July, 2021",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:828/format:webp/1*wyVMcBwA8COujHeMbikiCw.jpeg",
+        link:"https://medium.com/catalysts-reachout/how-to-become-successful-freelancer-in-college-aafc1125b403"
+    },
+    {
+        id:4,
+        name:"What Does an UX Designer Actually Do?",
+        tags:["ux","ui","wireframe"],
+        date:"26 May, 2021",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:828/format:webp/1*JWStOGfx3DvZDDphuMNvgQ.jpeg",
+        link:"https://medium.com/catalysts-reachout/what-does-a-ux-designer-actually-do-d488dac2cf03"
+    }
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Digi Ayurveda",
+        description:"An Ayurveda doctor consultation website connects users with certified Ayurveda doctors for online consultations, providing treatmentrecommendations and additionalresources",
+        tags:[ "React.js", "Redux", "Node.js", "Express.js", "Tailwind"],
+        
+        demo:"https://digi-ayu.vercel.app/",
+        github:"https://github.com/CatalystsReachOut/digiAyu"
+    },
+    {
+        id:2,
+        name:"Portfolio website",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","sass"],
+        
+        demo:"https://www.shubhangijadhav.com/",
+        github:"https://github.com/codebucks27/react-portfolio-final?ref=reactjsexample.com"
+        
+    },
+    {
+        id:3,
+        name:"IT REPEATS",
+        description:"A web application that enables college students to upload and access test questions from earlier years in orderto prepare for exams.",
+        tags:["react","Tailwind Css","sass","Node.js"],
+        
+        // demo:"http://devto-clone-codebucks27.vercel.app/",
+        github:"https://github.com/CatalystsReachOut/it-repeats-web"
+    },
+    {
+        id:4,
+        name:"Sidebar Menu",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://react-sidebar.vercel.app/",
+        github:"https://github.com/codebucks27/react-sidebar"
+       
+    },{
+        id:5,
+        name:"Todo App(React + Redux)",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        
+    },
+    {
+        id:6,
+        name:"Responsive Navigation Menu",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["react","react-router","css"],
+        
+        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/React-responsive-navbar"
+        
+    },
+    
+  
+] 
